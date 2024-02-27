d2598cdf31e5896c147b0b91d916145fdac588cc
Nanook123 <joshallfordnanook@gmail.com>
2024-02-27 17:18:14 +0000

    frontend and backend initial

---
+++ ayaotd/ayaotd-frontend/src/message.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Message({ messages }:{messages: string[]}){
+    return <div> {
+        messages.map((message, index) =>
+        <div key={index}>{message}</div>
+        )
+        }
+
+    </div>
+        
+}
